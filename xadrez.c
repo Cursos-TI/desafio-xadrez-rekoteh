cc96bd5c557652c198bec3aa1c5cb3f9b8b2d9e4
Edla Goveia <107441673+rekoteh@users.noreply.github.com>
2025-04-24 21:14:25 +0000

    Add desafio mestre

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -27,6 +25,71 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-
+    
+    #include <stdio.h>
+    //Lógica do Bispo de  Recursividade.
+    void recursivoBispo(int casas_bispo_restantes) {
+        if(casas_bispo_restantes > 0){
+            printf("Cima\n");
+            printf("Direita\n");
+            recursivoBispo(casas_bispo_restantes - 1);      
+    }
+    //Lógica da Torre de Recursividade.
+    }
+    void recursivoTorre(int casas_torre_restantes) {
+        if(casas_torre_restantes > 0){
+            printf("Direita\n");
+            recursivoTorre(casas_torre_restantes - 1);      
+    }
+    // Lógica da Rainha de Recursividade. 
+    }
+    void recursivoRainha_Horizontal(int casas) {
+        if(casas > 0) {
+            printf("Esquerda\n");
+            recursivoRainha_Horizontal(casas - 1);
+        }
+    }
+    
+    int main () {
+        
+        //Movimento da Torre...
+        printf("Jogo do Xadrez...\n");
+        printf("---Possibilidades da Torre---\n");
+        printf("5 casas para a direita.\n");
+        int casas_torre = 5;
+        recursivoTorre(casas_torre);
+    
+        //Movimento do Bispo...
+        printf("---Possibilidades do Bispo ---\n");
+        printf("---Versão Recursiva---\n");
+        printf("5 na diagonal (cima/direita).\n");
+        recursivoBispo(5);
+    
+        // código dos loops
+        printf("---Versão com Loops Aninhados---\n");
+        printf("5 na diagonal (cima/direita).\n");
+        for(int i = 0; i < 5;i++){
+            printf("Cima\n");
+         for(int j = 0; j < 1; j++){
+            printf("Direita\n");
+         }    
+        }
+        //Movimento da Rainha
+        printf("---Possibilidades da Rainha ---\n");
+        printf("8 casas para a esquerda.\n");//Direção do movimento da Rainha.
+        recursivoRainha_Horizontal(8);
+        //Movimento do Cavalo
+      
+        printf("\n---Possibilidades do Cavalo ---\n");
+        printf("2 casas para cima e 1 para direita (em L).\n");//1 Direção do movimento do Cavalo.
+        for (int i = 0; i < 3; i++) {
+            if(i < 2) {
+            printf("Cima\n");
+            continue;
+            }
+            printf("Direita\n");
+        }    
+    
     return 0;
-}
+    }
+ 
